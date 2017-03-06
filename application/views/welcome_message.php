9116abae50629c24fb84b1f64a9bf2bd3676d6c6
TigerFang <tigerfun@126.com>
2017-03-06 16:56:56 +0800

    Change of the original welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,12 +68,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to Tiger's CodeIgniter Test Site!</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>Hello World!</code>
+		<code>This message was added by Tiger @ 20171655 from Wangsu Shanghai</code>
 		<code>application/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
